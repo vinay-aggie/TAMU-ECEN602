340932135f2e985d5befcee8e4c0c48b34f700ba
vinay-aggie <vinay_b@tamu.edu>
2024-09-26 22:53:03 -0500

    Create socketUtils.h

---
+++ socketUtils.h
@@ -0,0 +1,36 @@
+// Header file.
+
+#ifndef SOCKET_UTILS_H
+#define SOCKET_UTILS_H
+
+#include <stdio.h>
+#include <stdint.h>
+
+#define MAX_USERNAME 128
+#define BUFFER_SIZE 1024
+
+// protocol version
+#define VERSION 3
+
+// header types
+#define JOIN 2
+#define SEND 4
+#define FWD 3
+
+// header length
+#define HEADER_LENGTH 4
+
+#define MAX_CONNECTIONS 100
+
+#define USERNAME 2
+#define MESSAGE 4
+#define REASON 1
+#define CLIENT_COUNT 3
+
+// Utility functions for socket handling
+void addAttribute(char* buff, uint16_t offset, uint16_t type, uint16_t length, const char* payload);
+int sendMessage(int socket, uint16_t version, uint16_t type, uint16_t length, const char *payload);
+int receiveMessage(int fd, char *buf, uint16_t* _version, uint16_t* _type, uint16_t* _length);
+void readAttribute(char *readBuff, char* writeBuff, uint16_t offset, uint16_t* _type, uint16_t* _length);
+
+#endif /* SOCKET_UTILS_H */
